764fe72441da84a5da4df54442b4353a26986e1e
Jonas Kruckenberg <jonas@crabnebula.dev>
2023-04-17 14:28:06 +0200

    feat: add style basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import compress from 'astro-compress'
+import {writeFileSync} from 'fs'
+import path from 'path'
+
+writeFileSync(
+    path.resolve('./src/styles/_env.scss'),
+    `$TAURI_PLATFORM: '${process.env.TAURI_PLATFORM}';
+  $TAURI_ARCH: '${process.env.TAURI_ARCH}';
+  $TAURI_FAMILY: '${process.env.TAURI_FAMILY}';
+  $TAURI_PLATFORM_VERSION: '${process.env.TAURI_PLATFORM_VERSION}';
+  $TAURI_PLATFORM_TYPE: '${process.env.TAURI_PLATFORM_TYPE}';
+  $TAURI_DEBUG: '${process.env.TAURI_DEBUG}';
+  `,
+  )
 
 // https://astro.build/config
 export default defineConfig({
