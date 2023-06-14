613973dc82accb64ffeb81c7d0f1bc191fafabfd
Jonas Kruckenberg <jonas@crabnebula.dev>
2023-06-14 15:31:49 +0200

    add long running async function to test-fixture

--- test-fixture/src/main.rs
+++ test-fixture/src/main.rs
@@ -4,8 +4,12 @@
 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::time::Duration;
+
 #[tauri::command]
-fn test(foo: &str) -> &str { foo }
+fn test(foo: &str) -> &str {
+    foo
+}
 
 #[tauri::command]
 async fn test2() {}
@@ -18,13 +22,18 @@ fn test4() -> Result<(), String> {
     Ok(())
 }
 
+#[tauri::command]
+async fn test5() {
+    tokio::time::sleep(Duration::from_secs(5)).await;
+}
+
 fn main() {
     let context = tauri::generate_context!();
 
     subscriber::init(&context);
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![test, test2, test3, test4])
+        .invoke_handler(tauri::generate_handler![test, test2, test3, test4, test5])
         .run(context)
         .expect("error while running tauri application");
 }
