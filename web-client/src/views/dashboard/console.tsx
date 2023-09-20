79265c5867f53649c2e02af4350aff279645b60a
Lucas Nogueira <lucas@crabnebula.dev>
2023-09-20 08:15:27 -0300

    fix log message type [skip ci]

--- web-client/src/views/dashboard/console.tsx
+++ web-client/src/views/dashboard/console.tsx
@@ -5,7 +5,7 @@ import { LOGS_UNWATCH, LOGS_WATCH } from "../../lib/requests";
 
 type WSEventSignal = Record<"message", MessageEvent<string>>;
 
-type LogResult = {
+type Log = {
   timestamp: number;
   message: string;
   target: string;
@@ -15,13 +15,13 @@ type LogResult = {
   line: number;
 };
 
-type Log = {
+type LogMessage = {
   id: string;
   jsonrpc: "2.0";
   method: "logs_added";
   params: {
     subscription: string;
-    result?: LogResult;
+    result?: Log[];
   };
 };
 
@@ -32,12 +32,13 @@ function formatTimestamp(stamp: Date) {
 export default function Console() {
   const { socket } = useWs();
   const message = createEventSignal<WSEventSignal>(socket, "message");
-  const [logs, setLogs] = createSignal<LogResult[]>([]);
+  const [logs, setLogs] = createSignal<Log[]>([]);
   const [currentSubscription, setCurrentSubscription] = createSignal();
 
-  const logData = (): Log | undefined => {
-    if (message()) {
-      return JSON.parse(message().data);
+  const logData = (): LogMessage | undefined => {
+    const m = message();
+    if (m) {
+      return JSON.parse(m.data);
     }
   };
 
@@ -45,7 +46,7 @@ export default function Console() {
     const log = logData()?.params?.result || null;
     if (log !== null) {
       setCurrentSubscription(log);
-      setLogs((prev) => [log, ...prev]);
+      setLogs((prev) => [...log, ...prev]);
     }
   });
 
