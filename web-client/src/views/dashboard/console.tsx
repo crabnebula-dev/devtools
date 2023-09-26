be41bd212a2e6b4a1f25f99d83ac184b3619c065
Atila Fassina <atila@crabnebula.dev>
2023-09-26 16:49:01 +0200

    tweak UI for the console panel

--- web-client/src/views/dashboard/console.tsx
+++ web-client/src/views/dashboard/console.tsx
@@ -1,21 +1,37 @@
-import { For } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 import { formatTimestamp } from "~/lib/formaters";
 import { useSocketData } from "~/lib/ws-store";
 
 export default function Console() {
   const { data } = useSocketData();
+  const [showTimestamp, toggleTimeStamp] = createSignal(true);
 
   return (
     <>
-      <ul>
+      <button
+        class="border border-neutral-200 rounded-lg py-px px-2"
+        type="button"
+        onClick={() => toggleTimeStamp((prev) => !prev)}
+      >
+        Show timestamp
+      </button>
+      <ul class="m-5 px-5 border border-neutral-800 rounded-md max-h-80 overflow-y-auto">
         <For each={data.logs}>
           {({ message, timestamp }) => {
             const timeDate = new Date(timestamp);
+
             return (
-              <li class="py-1">
-                <time dateTime={timeDate.toISOString()} class="font-mono pr-4">
-                  {formatTimestamp(timeDate)}
-                </time>
+              <li class="py-1 flex ">
+                <Show when={showTimestamp()}>
+                  <div class="text-right min-w-[9em]">
+                    <time
+                      dateTime={timeDate.toISOString()}
+                      class="font-mono pr-4"
+                    >
+                      {formatTimestamp(timeDate)}
+                    </time>
+                  </div>
+                </Show>
                 <span>{message}</span>
               </li>
             );
