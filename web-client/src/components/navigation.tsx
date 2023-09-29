b2b601b482791d055ef5fdf31660b00279a07247
Tejas Kumar <tejas@tejas.qa>
2023-09-29 17:45:49 +0200

    Adjust nav and console a bit

--- web-client/src/components/navigation.tsx
+++ web-client/src/components/navigation.tsx
@@ -24,14 +24,14 @@ export function Navigation() {
   const { wsUrl, wsPort } = useParams();
   return (
     <nav>
-      <ul class="flex flex-start border-b-neutral-800">
+      <ul class="flex border-b flex-start border-b-neutral-800">
         <For each={TABS}>
           {(tab) => (
             <li>
               <A
                 href={tab.url(wsUrl, wsPort)}
-                class="border-black bg-black flex items-center justify-center leading-none border-b-2 py-2 px-4 hover:bg-gray-800 hover:border-gray-800"
-                activeClass="border-gray-600 hover:border-gray-500"
+                class="flex border-b-neutral-800 -mb-[1px] items-center justify-center leading-none border-b py-2 px-4 hover:bg-gray-800 hover:border-gray-800"
+                activeClass="border-b-gray-600 hover:border-b-gray-500"
               >
                 {tab.title}
               </A>
