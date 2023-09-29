a61b27b374b477f68306093504abba22efd68e95
Tejas Kumar <tejas@tejas.qa>
2023-09-29 17:44:07 +0200

    Add CrabNebula gradient

--- web-client/tailwind.config.ts
+++ web-client/tailwind.config.ts
@@ -3,7 +3,11 @@ import scrollbar from "tailwind-scrollbar";
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#0C1326"
+      }
+    },
   },
   plugins: [scrollbar({ nocompatible: true })],
 };
