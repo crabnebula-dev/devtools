9e8a5e6b45e91aa72e078cde51ddb36da0c92890
Tejas Kumar <tejas@tejas.qa>
2023-11-02 10:18:00 +0100

    UI a11y pass

--- web-client/src/components/navigation.tsx
+++ web-client/src/components/navigation.tsx
@@ -1,5 +1,6 @@
 import { For } from "solid-js";
-import { A, useParams } from "@solidjs/router";
+import { A, useParams, useLocation } from "@solidjs/router";
+import clsx from "clsx";
 
 const TABS = [
   {
@@ -22,6 +23,7 @@ const TABS = [
 
 export function Navigation() {
   const { host, port } = useParams();
+  const location = useLocation();
   return (
     <nav>
       <ul class="flex border-b flex-start border-b-neutral-800">
@@ -29,9 +31,27 @@ export function Navigation() {
           {(tab) => (
             <li>
               <A
+                // Maximum a11y: respond to both Enter _and_ Space
+                // Buttons natively do this, anchor links not but we
+                // want these to behave like buttons also.
+                // Ref: https://www.w3.org/WAI/ARIA/apg/patterns/button
+                onkeydown={(e) => {
+                  if (e.key !== " ") {
+                    return;
+                  }
+
+                  e.preventDefault();
+                  e.currentTarget.click();
+                }}
                 href={tab.url(host, port)}
-                class="flex border-b-neutral-800 -mb-[1px] items-center justify-center leading-none border-b py-2 px-4 hover:bg-gray-800 hover:border-gray-800"
-                activeClass="border-b-gray-600 hover:border-b-gray-500"
+                class={clsx(
+                  location.pathname === tab.url(host, port)
+                    ? "border-b-gray-500 hover:border-b-gray-400"
+                    : "border-b-gray-800 hover:border-b-gray-600",
+
+                  // The rest
+                  "flex -mb-[1px] items-center justify-center leading-none border-b py-2 px-4 hover:bg-gray-800 hover:border-gray-800"
+                )}
               >
                 {tab.title}
               </A>
