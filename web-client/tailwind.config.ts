c36999868f3dc0bf0b2b809aecffbf15caff07ca
Tejas Kumar <tejas@tejas.qa>
2023-11-10 11:23:20 +0100

    Update tailwind config

--- web-client/tailwind.config.ts
+++ web-client/tailwind.config.ts
@@ -1,5 +1,5 @@
 import scrollbar from "tailwind-scrollbar";
-import defaultTheme from "tailwindcss/defaultTheme";
+import solar from "@crabnebula/solar"
 
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
@@ -8,25 +8,7 @@ export default {
       spacing: {
         toolbar: 'var(--toolbar-height)',
       },
-      fontFamily: {
-        'sans': ['Euclid Circular B', 'Inter', ...defaultTheme.fontFamily.sans]
-      },
-      colors: {
-        "navy": {
-          50: "#DCE2F4",
-          100: "#B9C6E9",
-          200: "#738DD3",
-          300: "#3859B2",
-          400: "#22366D",
-          500: "#0C1326",
-          600: "#0A0F1F",
-          700: "#070C17",
-          800: "#050810",
-          900: "#020408",
-          950: "#010204"
-        }
-      }
-    },
+    }
   },
-  plugins: [scrollbar({ nocompatible: true })],
+  plugins: [solar, scrollbar({ nocompatible: true })],
 };
