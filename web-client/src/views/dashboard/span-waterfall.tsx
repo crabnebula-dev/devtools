0605194b92f6b19b13a2b4a7eac2d8eeb68eed8f
Tejas Kumar <tejas@tejas.qa>
2023-11-10 18:42:33 +0100

    Add basic waterfall UI

--- web-client/src/views/dashboard/span-waterfall.tsx
+++ web-client/src/views/dashboard/span-waterfall.tsx
@@ -1,118 +1,78 @@
-import { For, createSignal, Show } from "solid-js";
-import { AutoscrollPane } from "~/components/autoscroll-pane";
-import { FilterToggle } from "~/components/filter-toggle";
+import { For, createEffect, createSignal } from "solid-js";
 import { useMonitor } from "~/lib/connection/monitor";
-import { Field } from "~/lib/proto/common";
 import { Toolbar } from "~/components/toolbar";
-
-function fieldValue(field: Field): string {
-  switch (field.value.oneofKind) {
-    case "debugVal":
-      return field.value.debugVal;
-    case "strVal":
-      return field.value.strVal;
-    case "u64Val":
-      return field.value.u64Val.toString();
-    case "i64Val":
-      return field.value.i64Val.toString();
-    case "boolVal":
-      return field.value.boolVal.toString();
-    case "doubleVal":
-      return field.value.doubleVal.toString();
-    default:
-      return "";
-  }
-}
-
-const serializer = (_: unknown, v: unknown) =>
-  typeof v === "bigint" ? v.toString() : v;
+import { normalizeSpans } from "~/lib/span/normalizeSpans";
+import { convertTimestampToNanoseconds } from "~/lib/formatters";
 
 export default function SpanWaterfall() {
   const { monitorData } = useMonitor();
-  const [shouldAutoScroll, toggleAutoScroll] = createSignal<boolean>(true);
+  const [granularity, setGranularity] = createSignal(1);
+  const [uiSpans, setUiSpans] = createSignal<ReturnType<typeof normalizeSpans>>(
+    []
+  );
 
-  const filteredSpans = () => {
-    const spans = monitorData.spans.filter((s) => {
+  const filteredSpans = () =>
+    monitorData.spans.filter((s) => {
       const metadata = monitorData.metadata.get(s.metadataId);
-      return (
-        metadata && ["tauri", "wry"].some((t) => metadata.target.startsWith(t))
-      );
+      return metadata && metadata.name.includes("ipc");
     });
 
-    return spans;
-  };
+  createEffect(() => {
+    setUiSpans(
+      normalizeSpans(
+        filteredSpans()
+          .map((span) => {
+            if (!span.enteredAt || !span.exitedAt) {
+              return { end: -1, start: -1 };
+            }
+            return {
+              start: convertTimestampToNanoseconds(span.enteredAt),
+              end: convertTimestampToNanoseconds(span.exitedAt),
+            };
+          })
+          .filter((span) => span.start !== -1 && span.end !== -1),
+        granularity() * 1000
+      )
+    );
+  });
 
   return (
-    <div class="px-5 ">
+    <div>
       <Toolbar>
-        <FilterToggle
-          aria-label="auto scroll"
-          defaultPressed
-          changeHandler={() => toggleAutoScroll((prev) => !prev)}
-        >
-          Autoscroll
-        </FilterToggle>
+        <input
+          oninput={(e) => setGranularity(parseFloat(e.target.value))}
+          type="range"
+          min="1"
+          max={5}
+          value={granularity()}
+        />
       </Toolbar>
-      <AutoscrollPane
-        dataStream={monitorData.spans[0]}
-        shouldAutoScroll={shouldAutoScroll}
-      >
+      <ul class="w-full overflow-auto h-[calc(100vh-114px)]">
         <For each={filteredSpans()}>
-          {(span) => {
+          {(span, i) => {
             const metadata = monitorData.metadata.get(span.metadataId);
 
             return (
-              <li class="py-1 flex w-full">
-                <pre class="border border-neutral-800 p-2 w-full overflow-x-auto">
-                  <p>ID: {span.id.toString()}</p>
-                  <p>
-                    (Target, Name): ({metadata?.target} /// {metadata?.name})
-                  </p>
-                  <p>
-                    Location: {metadata?.location?.file}@
-                    {metadata?.location?.line}:{metadata?.location?.column}
-                  </p>
-                  <p>Created: {JSON.stringify(span.createdAt, serializer)}</p>
-                  <p>Entered: {JSON.stringify(span.enteredAt, serializer)}</p>
-                  <p>Exited: {JSON.stringify(span.exitedAt, serializer)}</p>
-                  <Show when={span.fields.length > 0}>
-                    <div>
-                      <p>Fields</p>
-                      <For each={span.fields}>
-                        {(field) => {
-                          return (
-                            <p>
-                              {field.name} = {fieldValue(field)}
-                            </p>
-                          );
-                        }}
-                      </For>
-                    </div>
-                  </Show>
-                  <Show when={span.children.length > 0}>
-                    <div>
-                      <p>Children</p>
-                      <For each={span.children}>
-                        {(s) => {
-                          return (
-                            <div>
-                              <p>Id: {s.id.toString()}</p>
-                              <p>
-                                Name:{" "}
-                                {monitorData.metadata.get(s.metadataId)?.name}
-                              </p>
-                            </div>
-                          );
-                        }}
-                      </For>
-                    </div>
-                  </Show>
-                </pre>
+              <li class="py-1 flex">
+                <div
+                  data-tooltip={`Took ${
+                    (convertTimestampToNanoseconds(span.exitedAt!) -
+                      convertTimestampToNanoseconds(span.enteredAt!)) /
+                    1e6
+                  }ms`}
+                  class="span relative transition-all text-xs p-1 overflow-ellipsis text-white bg-primary-800 rounded"
+                  style={{
+                    width: `${uiSpans()[i()]?.normalizedWidth}%`,
+                    "margin-left": `${uiSpans()[i()]?.normalizedStart}%`,
+                  }}
+                >
+                  {metadata?.name}
+                </div>
               </li>
             );
           }}
         </For>
-      </AutoscrollPane>
+      </ul>
     </div>
   );
 }
