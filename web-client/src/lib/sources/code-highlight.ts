c3429b762b4d8f1a263edeb37c7dcaae27c29575
Atila Fassina <atila@crabnebula.dev>
2023-11-13 11:02:27 +0100

    move `shiki` stuff to its own file

---
+++ web-client/src/lib/sources/code-highlight.ts
@@ -0,0 +1,58 @@
+import { Highlighter, Lang, getHighlighter, setCDN, setWasm } from "shiki";
+import { SourcesClient } from "~/lib/proto/sources.client";
+import { getEntryBytes } from "./util";
+
+type HighliterCodeParams = Readonly<
+  [SourcesClient, string, number, HighlighterLang]
+>;
+
+export const SUPPORTED_LANGS = [
+  "js",
+  "rust",
+  "toml",
+  "html",
+  "json",
+  "md",
+  "yaml",
+] satisfies Lang[];
+
+type LangList = typeof SUPPORTED_LANGS;
+export type HighlighterLang = LangList[number];
+
+// Initializing a TextDecoder is expensive plus they can be reused,
+// so we create a global instance
+const TEXT_DECODER = new TextDecoder();
+
+export function bytesToHtml(
+  bytes: Uint8Array | undefined,
+  lang: HighlighterLang,
+  highlighter: Highlighter | undefined
+) {
+  if (!bytes || !highlighter) return;
+
+  const text = TEXT_DECODER.decode(bytes);
+
+  return highlighter.codeToHtml(text, { lang });
+}
+
+export async function getHighlightedCode([
+  sourcesClient,
+  path,
+  size,
+  lang,
+]: HighliterCodeParams) {
+  setCDN("/shiki/");
+  const responseWasm = await fetch("/shiki/onig.wasm");
+  setWasm(responseWasm);
+
+  const highlither = await getHighlighter({
+    theme: "material-theme-ocean",
+    langs: SUPPORTED_LANGS,
+    paths: { wasm: "dist/" },
+  });
+
+  const bytes = await getEntryBytes(sourcesClient, path, size);
+  const code = bytesToHtml(bytes, lang, highlither);
+
+  return code;
+}
