00543e00d27420a281fb8ba780d8a157d5bfa510
Atila Fassina <atila@crabnebula.dev>
2023-11-13 19:29:58 +0100

    remove useless `<Suspense>` component

--- web-client/src/components/sources/image-view.tsx
+++ web-client/src/components/sources/image-view.tsx
@@ -1,8 +1,7 @@
-import { createResource, Suspense } from "solid-js";
+import { createResource, Show } from "solid-js";
 import { useRouteData } from "@solidjs/router";
 import { Connection } from "~/lib/connection/transport.ts";
 import { decodeFileName, getEntryBytes } from "~/lib/sources/file-entries.ts";
-import { Loader } from "~/components/loader";
 
 export function ImageView(props: { path: string; size: number; type: string }) {
   const { client } = useRouteData<Connection>();
@@ -11,16 +10,20 @@ export function ImageView(props: { path: string; size: number; type: string }) {
     ([client, path, size]) => getEntryBytes(client, decodeFileName(path), size)
   );
 
-  const url = () =>
-    URL.createObjectURL(new Blob([bytes()!], { type: props.type }));
+  const url = () => {
+    const b = bytes();
+
+    if (!b) return;
+    return URL.createObjectURL(new Blob([b], { type: props.type }));
+  };
 
   return (
-    <Suspense fallback={<Loader />}>
+    <Show when={url()}>
       <img
         class="max-w-full max-h-full"
         src={url()}
         alt={`image for ${props.path}`}
       />
-    </Suspense>
+    </Show>
   );
 }
