6166964dae4d28205a5352664e1fa7a960fc5942
Lucas Nogueira <lucas@crabnebula.dev>
2023-11-14 13:01:58 -0300

    find command name

--- web-client/src/views/dashboard/span-waterfall.tsx
+++ web-client/src/views/dashboard/span-waterfall.tsx
@@ -1,4 +1,4 @@
-import { useMonitor } from "~/lib/connection/monitor";
+import { Span, useMonitor } from "~/lib/connection/monitor";
 import { Toolbar } from "~/components/toolbar";
 import { For, createEffect, createSignal } from "solid-js";
 import { convertTimestampToNanoseconds, formatDate } from "~/lib/formatters";
@@ -46,12 +46,41 @@ export default function SpanWaterfall() {
             {(span, i) => {
               const metadata = () =>
                 monitorData.metadata.get(spans()[i()].metadataId);
+
+              function findChild(span: Span, name: string): Span | null {
+                const meta = monitorData.metadata.get(span.metadataId);
+                if (meta?.name === name) {
+                  return span;
+                } else {
+                  for (const child of span.children) {
+                    const s = findChild(child, name);
+                    if (s) {
+                      return s;
+                    }
+                  }
+                }
+                return null;
+              }
+
+              const name = () => {
+                const meta = metadata();
+                if (meta?.name === "wry::ipc::handle") {
+                  const span = spans()[i()];
+                  const commandHandlerSpan = findChild(span, "ipc::request::handle");
+                  if (commandHandlerSpan) {
+                    const val = commandHandlerSpan.fields.find((f) => f.name === 'cmd')?.value;
+                    // this is actuall always strVal unless the Tauri tracing implementation messes it up
+                    return val?.oneofKind === 'strVal' ? val.strVal : null;
+                  }
+                }
+                return meta?.name;
+              }
               const createdAt = convertTimestampToNanoseconds(span.createdAt!);
               const exitedAt = convertTimestampToNanoseconds(span.exitedAt!);
               const enteredAt = convertTimestampToNanoseconds(span.enteredAt!);
               return (
                 <tr class="even:bg-[#ffffff09] cursor-pointer hover:bg-[#ffffff05] even:hover:bg-[#ffffff10]">
-                  <td class="p-1">{metadata()!.name}</td>
+                  <td class="p-1">{name()}</td>
                   <td class="p-1">{metadata()?.target}</td>
                   <td class="p-1">
                     {formatDate(new Date(createdAt / 1000000))}
