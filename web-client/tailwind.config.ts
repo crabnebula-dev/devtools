a05079a80dad49063bbe96a4b58a3f920c2837d7
Atila Fassina <atila@crabnebula.dev>
2023-11-15 11:37:29 +0100

    add `.gutter` to safelist

--- web-client/tailwind.config.ts
+++ web-client/tailwind.config.ts
@@ -1,18 +1,24 @@
+import type { Config } from "tailwindcss";
 import scrollbar from "tailwind-scrollbar";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    // split.js classes
+    "gutter",
+    "gutter-horizontal",
+  ],
   theme: {
     extend: {
       spacing: {
-        toolbar: 'var(--toolbar-height)',
+        toolbar: "var(--toolbar-height)",
       },
       fontFamily: {
-        'sans': ['Euclid Circular B', 'Inter', ...defaultTheme.fontFamily.sans]
+        sans: ["Euclid Circular B", "Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        "navy": {
+        navy: {
           50: "#DCE2F4",
           100: "#B9C6E9",
           200: "#738DD3",
@@ -23,10 +29,10 @@ export default {
           700: "#070C17",
           800: "#050810",
           900: "#020408",
-          950: "#010204"
-        }
-      }
+          950: "#010204",
+        },
+      },
     },
   },
   plugins: [scrollbar({ nocompatible: true })],
-};
+} satisfies Config;
