998c3cd2aa1aa6c744ed80a268bfd2c52108b30b
Jonas Kruckenberg <118265418+CrabNejonas@users.noreply.github.com>
2023-11-15 11:25:30 +0100

    disable recorder

--- devtools/src/lib.rs
+++ devtools/src/lib.rs
@@ -118,16 +118,9 @@ pub fn try_init<R: Runtime>() -> Result<tauri::plugin::TauriPlugin<R>> {
     let (event_tx, event_rx) = mpsc::channel(512);
     let (cmd_tx, cmd_rx) = mpsc::channel(256);
 
-    let recorder = if std::env::args().nth(1) == Some("--record".to_string()) {
-        let path = PathBuf::from(std::env::args().nth(2).ok_or(Error::MissingRecordingPath)?);
-        Some(Recorder::new(&path)?)
-    } else {
-        None
-    };
-
     // set up components
     let layer = Layer::new(shared.clone(), event_tx);
-    let aggregator = Aggregator::new(shared, event_rx, cmd_rx, recorder);
+    let aggregator = Aggregator::new(shared, event_rx, cmd_rx, None);
 
     // initialize early so we don't miss any spans
     tracing_subscriber::registry()
