e4d845ead8b2b3c5f5050995859499ffeed8204d
Tejas Kumar <tejas@tejas.qa>
2023-11-16 01:42:47 +0200

    Add Span Detail

--- web-client/src/lib/span/formatSpansForUi.ts
+++ web-client/src/lib/span/formatSpansForUi.ts
@@ -1,5 +1,6 @@
 import { Span } from "../connection/monitor";
 import { Metadata } from "../proto/common";
+import { flattenChildren } from "./flattenChildren";
 import { getIpcRequestName } from "./getIpcRequestName";
 import { normalizeSpans } from "./normalizeSpans";
 
@@ -7,16 +8,20 @@ type Options = {
     spans: Span[]
     metadata: Map<bigint, Metadata>
 }
-export function formatSpansForUi({ spans, metadata }: Options) {
-    return normalizeSpans(spans).map(span => {
 
+export function formatSpansForUi({ spans, metadata }: Options): any {
+    const result = normalizeSpans(spans).map(span => {
         return ({
+            id: String(span.id),
             name: getIpcRequestName({ metadata, span }) || '-',
             initiated: span.createdAt / 1000000,
             time: span.closedAt ? (span.closedAt - span.createdAt) / 1e6 : 0,
             waterfall: `width:${span.width}%;margin-left:${span.marginLeft}%;`,
             start: span.marginLeft,
-            slices: span.slices.map(slice => `width:${slice.width}%;margin-left:${slice.marginLeft}%;`)
+            slices: span.slices.map(slice => `width:${slice.width}%;margin-left:${slice.marginLeft}%;`),
+            children: formatSpansForUi({ spans: flattenChildren(span.children), metadata })
         })
     })
+
+    return result;
 }
