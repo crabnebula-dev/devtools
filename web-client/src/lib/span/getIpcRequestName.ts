91629eb1dbc4f9ec59094a95de6dae5e19273b1d
Tejas Kumar <tejas@tejas.qa>
2023-11-16 02:10:24 +0200

    Test everything

--- web-client/src/lib/span/getIpcRequestName.ts
+++ web-client/src/lib/span/getIpcRequestName.ts
@@ -1,6 +1,6 @@
 import { Span } from "../connection/monitor";
 import { Metadata } from "../proto/common";
-import { findSpanByName } from "./findSpanByName";
+import { recursivelyFindSpanByName } from "./recursivelyFindSpanByName";
 
 type Options = {
     metadata: Map<bigint, Metadata>
@@ -8,12 +8,12 @@ type Options = {
 }
 
 export function getIpcRequestName({ metadata, span }: Options) {
-    const meta = metadata.get(span.metadataId)!;
-    if (meta.name === "wry::ipc::handle") {
-        const commandHandlerSpan = findSpanByName(
+    const meta = metadata.get(span.metadataId);
+    if (meta?.name === "wry::ipc::handle") {
+        const commandHandlerSpan = recursivelyFindSpanByName(
             { span, metadata },
             "ipc::request::handle"
-        );
+        )?.[0] ?? null;
         if (commandHandlerSpan) {
             const val = commandHandlerSpan.fields.find(
                 (f) => f.name === "cmd"
@@ -22,5 +22,5 @@ export function getIpcRequestName({ metadata, span }: Options) {
             return val?.oneofKind === "strVal" ? val.strVal : null;
         }
     }
-    return meta.name;
+    return meta?.name ?? null;
 };
---
+++ web-client/src/lib/span/tests/formatSpansForUi.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect } from "vitest";
+import { formatSpansForUi } from "../formatSpansForUi";
+import { spans } from "./fixtures/spans"
+import { metadata } from "./fixtures/metadata"
+
+describe("formatSpansForUi", () => {
+    it("should format spans for UI", () => {
+        // @ts-expect-error this is a test...
+        expect(formatSpansForUi({ spans, metadata })).toMatchSnapshot()
+    })
+})
