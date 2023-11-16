3ad5530bb78c950a8cb282999f0f314936740896
Tejas Kumar <tejas@tejas.qa>
2023-11-16 02:17:33 +0200

    Prettier

--- web-client/src/lib/span/tests/recursivelyFindSpanByName.test.ts
+++ web-client/src/lib/span/tests/recursivelyFindSpanByName.test.ts
@@ -4,10 +4,10 @@ import { metadata } from "./fixtures/metadata";
 import { spans } from "./fixtures/spans";
 
 describe("recursivelyFindSpanByName", () => {
-    it("should recursively find a span by name", () => {
-        expect(
-            // @ts-expect-error this is a test...
-            recursivelyFindSpanByName({ span: spans[0], metadata }, "cmd")
-        ).toMatchSnapshot();
-    });
+  it("should recursively find a span by name", () => {
+    expect(
+      // @ts-expect-error this is a test...
+      recursivelyFindSpanByName({ span: spans[0], metadata }, "cmd")
+    ).toMatchSnapshot();
+  });
 });
