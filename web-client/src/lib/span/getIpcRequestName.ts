5f223c5f1c316c8a65655afc1f016e044c05a772
Lucas Nogueira <lucas@crabnebula.dev>
2023-11-15 22:38:40 -0300

    format deserialize_arg span name

--- web-client/src/lib/span/getIpcRequestName.ts
+++ web-client/src/lib/span/getIpcRequestName.ts
@@ -1,5 +1,6 @@
 import { Span } from "../connection/monitor";
 import { Metadata } from "../proto/common";
+import { processFieldValue } from "./processFieldValue";
 import { recursivelyFindSpanByName } from "./recursivelyFindSpanByName";
 
 type Options = {
@@ -21,6 +22,11 @@ export function getIpcRequestName({ metadata, span }: Options) {
       )?.value;
       // this is actually always strVal unless the Tauri tracing implementation messes it up
       return val?.oneofKind === "strVal" ? val.strVal : null;
+    }
+  } else if (meta?.name === "ipc::request::deserialize_arg") {
+    const argField = span.fields.find((f) => f.name === "arg");
+    if (argField) {
+      return `deserialize arg ${processFieldValue(argField.value)}`
     }
   }
   return meta?.name ?? null;
