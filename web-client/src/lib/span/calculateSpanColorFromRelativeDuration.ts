597bb2e5db9aba631d7189a45378874e7c1be9a7
Tejas Kumar <tejas@tejas.qa>
2023-11-16 18:07:56 +0200

    Prettier

--- web-client/src/lib/span/calculateSpanColorFromRelativeDuration.ts
+++ web-client/src/lib/span/calculateSpanColorFromRelativeDuration.ts
@@ -1,10 +1,10 @@
 export function calculateSpanColorFromRelativeDuration(inputValue: number) {
-    if (inputValue > 150) {
-        return "bg-red-500"
-    }
-    if (inputValue > 100) {
-        return "bg-yellow-400"
-    }
+  if (inputValue > 150) {
+    return "bg-red-500";
+  }
+  if (inputValue > 100) {
+    return "bg-yellow-400";
+  }
 
-    return "bg-teal-500"
+  return "bg-teal-500";
 }
