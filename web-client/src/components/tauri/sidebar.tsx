77a84187ecdec820765fb3ba3ca244c10b95e650
Johann Derdak <johann@crabnebula.dev>
2023-11-22 15:50:48 +0100

    fix: change em to rem

--- web-client/src/components/tauri/sidebar.tsx
+++ web-client/src/components/tauri/sidebar.tsx
@@ -23,7 +23,7 @@ export function Sidebar() {
 function Config(props: { config: configurationObject }) {
   return (
     <section class="p-2">
-      <div class="grid gap-1.5 items-center text-left grid-cols-[1em_1fr] text-xl">
+      <div class="grid gap-1.5 items-center text-left grid-cols-[1rem_1fr] text-xl">
         <FileIcon path="tauri.conf.json" />
         {props.config.label}
       </div>
@@ -48,7 +48,7 @@ function TabLink(props: { name: string; key: string; size: number }) {
     <A
       href={`${basePath}/${props.key}/${props.name}?size=${props.size}`}
       activeClass="hover:bg-[#eaebeb] hover:border-[#2DCC9F] bg-[#00555A] border-[#2DCC9F] text-white"
-      class="text-lg border-b-2 hover:bg-[#00555A] hover:border-[#2DCC9F] p-1 border-neutral-800 text-neutral-400 hover:text-white grid gap-1.5 items-center text-left grid-cols-[1em_1fr]"
+      class="text-lg border-b-2 hover:bg-[#00555A] hover:border-[#2DCC9F] p-1 border-neutral-800 text-neutral-400 hover:text-white grid gap-1.5 items-center text-left grid-cols-[1rem_1fr]"
     >
       <FileIcon path={props.name} />
       {props.name}
