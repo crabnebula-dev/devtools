dc2462d5b0b7711b8dabe7f0b02c88f44e3a7d25
Johann Derdak <johann@crabnebula.dev>
2023-11-22 08:11:40 +0100

    fix: refactor sidebar html

--- web-client/src/components/tauri/sidebar.tsx
+++ web-client/src/components/tauri/sidebar.tsx
@@ -23,10 +23,8 @@ export function Sidebar() {
 function Config(props: { config: configurationObject }) {
   return (
     <section class="p-2">
-      <div class="flex text-xl items-center">
-        <div class="w-10 px-2">
-          <FileIcon path="tauri.conf.json" />
-        </div>
+      <div class="grid gap-1.5 items-center text-left grid-cols-[1em_1fr] text-xl">
+        <FileIcon path="tauri.conf.json" />
         {props.config.label}
       </div>
       <nav class="flex flex-col pl-8">
@@ -50,11 +48,9 @@ function TabLink(props: { name: string; key: string; size: number }) {
     <A
       href={`${basePath}/${props.key}/${props.name}?size=${props.size}`}
       activeClass="hover:bg-[#eaebeb] hover:border-[#2DCC9F] bg-[#00555A] border-[#2DCC9F] text-white"
-      class="text-lg border-b-2 hover:bg-[#00555A] hover:border-[#2DCC9F] p-1 border-neutral-800 text-neutral-400 hover:text-white"
+      class="text-lg border-b-2 hover:bg-[#00555A] hover:border-[#2DCC9F] p-1 border-neutral-800 text-neutral-400 hover:text-white grid gap-1.5 items-center text-left grid-cols-[1em_1fr]"
     >
-      <div class="inline-block w-6 align-middle mx-2">
-        <FileIcon path={props.name} />
-      </div>
+      <FileIcon path={props.name} />
       {props.name}
     </A>
   );
