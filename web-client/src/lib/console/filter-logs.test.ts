d31d502da95d3d0a91635b09e12620593aff2744
Tejas Kumar <tejas@tejas.qa>
2023-11-23 13:02:25 +0100

    Filter also on target and location

--- web-client/src/lib/console/filter-logs.test.ts
+++ web-client/src/lib/console/filter-logs.test.ts
@@ -3,61 +3,53 @@ import { fakeMonitorData } from "./fixtures/monitor-data";
 import { getLogMetadata } from "./get-log-metadata";
 
 describe("Log Filter Function", () => {
-  // This is a test, so `any` should be fine.
   const logs: any = fakeMonitorData.logs;
 
+  // Test with no filters
   it("should return all logs when no filter is applied", () => {
-    const result = filterLogs(
-      fakeMonitorData,
-      { textContent: "", levels: [] },
-      logs
-    );
+    const result = filterLogs(fakeMonitorData, { textContent: "", levels: [] }, logs);
     expect(result).toEqual(logs);
   });
 
-  it("should filter logs by text content", () => {
-    const textContent = "someText";
-    const expected = logs.filter((log: any) =>
-      log.message.includes(textContent)
-    );
-    const result = filterLogs(
-      fakeMonitorData,
-      { textContent, levels: [] },
-      logs
-    );
-    expect(result).toEqual(expected);
-  });
-
-  // ... more tests for each level
+  // Test each level individually
   for (let level = 0; level <= 4; level++) {
-    it(`should filter logs by level ${level}`, () => {
+    it(`should filter logs by level ${level} only`, () => {
       const expected = logs.filter(
         (log: any) => getLogMetadata(fakeMonitorData, log)?.level === level
       );
-      const result = filterLogs(
-        fakeMonitorData,
-        { textContent: "", levels: [level] },
-        logs
-      );
+      const result = filterLogs(fakeMonitorData, { textContent: "", levels: [level] }, logs);
       expect(result).toEqual(expected);
     });
   }
 
-  // ... tests for combined text content and levels
-  for (let level = 0; level <= 4; level++) {
-    it(`should filter logs by text content and level ${level}`, () => {
-      const textContent = "someText";
+  // Test with each possible textContent
+  const textContents = ["tauri", "hyper", "error"]; // Add more as needed
+  textContents.forEach((textContent) => {
+    it(`should filter logs by textContent '${textContent}'`, () => {
       const expected = logs.filter(
         (log: any) =>
-          log.message.includes(textContent) &&
-          getLogMetadata(fakeMonitorData, log)?.level === level
+          log.message.includes(textContent) ||
+          getLogMetadata(fakeMonitorData, log)?.target?.includes(textContent) ||
+          getLogMetadata(fakeMonitorData, log)?.location?.file?.includes(textContent)
       );
-      const result = filterLogs(
-        fakeMonitorData,
-        { textContent, levels: [level] },
-        logs
-      );
+      const result = filterLogs(fakeMonitorData, { textContent, levels: [] }, logs);
       expect(result).toEqual(expected);
     });
-  }
+
+    // Combined tests with each level and textContent
+    for (let level = 0; level <= 4; level++) {
+      it(`should filter logs by textContent '${textContent}' and level ${level}`, () => {
+        const expected = logs.filter(
+          (log: any) =>
+            (log.message.includes(textContent) ||
+              getLogMetadata(fakeMonitorData, log)?.target?.includes(textContent) ||
+              getLogMetadata(fakeMonitorData, log)?.location?.file?.includes(textContent)) &&
+            getLogMetadata(fakeMonitorData, log)?.level === level
+        );
+        const result = filterLogs(fakeMonitorData, { textContent, levels: [level] }, logs);
+        expect(result).toEqual(expected);
+      });
+    }
+  });
+
 });
