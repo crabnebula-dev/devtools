d2587212afa1e6053353267705fd9efe4cfce63c
Johann Derdak <johann@crabnebula.dev>
2023-11-28 15:05:13 +0100

    fix: move function to its own file

--- web-client/src/lib/tauri/tauri-conf-schema.ts
+++ web-client/src/lib/tauri/tauri-conf-schema.ts
@@ -20,6 +20,7 @@ import {
 import { zodSchemaForVersion } from "./config/zod-schema-for-version";
 import { getVersions } from "../connection/getters";
 import { z } from "zod";
+import { findLineNumberByNestedKeyInSource } from "./find-line-number-by-nested-key-in-source";
 
 export type ConfigurationStore = {
   configs?: ConfigurationObject[];
@@ -201,72 +202,6 @@ export function findLineNumberByKey(key: string) {
   const params = useParams<{ config: string }>();
   const config = retrieveConfigurationByKey(params.config);
   return findLineNumberByNestedKeyInSource(config?.raw ?? "", key);
-}
-
-export function findLineNumberByNestedKeyInSource(
-  jsonString: string,
-  nestedKeyPath: string
-): number {
-  const lines = jsonString.split("\n");
-  if (nestedKeyPath === "" || nestedKeyPath === undefined) return -1;
-  const searchStack = nestedKeyPath.split(".");
-  // Number of whitespaces used for indentation
-  const Indent = 2;
-
-  let currentLine = 1;
-  const keyStack: string[] = [];
-  const searchIndents: number[] = [0];
-  let searchLevel = 0;
-
-  let arrayCounter = 0;
-
-  for (const line of lines) {
-    const currentIndent = line.length - line.trimStart().length;
-    // If a property is closed we move up a level
-    if (keyStack.length > 0 && searchIndents[searchLevel] === currentIndent) {
-      keyStack.pop();
-      searchLevel--;
-      searchIndents.pop();
-      arrayCounter = 0;
-    }
-
-    // We make sure we only move into nested properties if the indent is correct
-    if (
-      searchIndents[searchLevel] === 0 ||
-      searchIndents[searchLevel] + Indent === currentIndent ||
-      searchIndents[searchLevel] + Indent + Indent === currentIndent
-    ) {
-      // If the search property matches we move down a level
-      if (line.includes('"' + searchStack[searchLevel] + '":')) {
-        keyStack.push(searchStack[searchLevel]);
-        searchLevel++;
-        searchIndents.push(currentIndent);
-      }
-
-      // If the search property is a number we are in an array and assume that we are in the correct spot and only have to search for the correct index
-      const arrayPointer = parseInt(searchStack[searchLevel]);
-      const numberOfValues = line.split(",").length;
-      if (Number.isInteger(arrayPointer)) {
-        if (
-          arrayCounter === arrayPointer ||
-          (numberOfValues > 1 && arrayPointer <= numberOfValues)
-        ) {
-          keyStack.push(searchStack[searchLevel]);
-          searchLevel++;
-          searchIndents.push(currentIndent);
-        } else {
-          arrayCounter++;
-        }
-      }
-    }
-
-    if (keyStack.length === searchStack.length) {
-      return currentLine;
-    }
-    currentLine++;
-  }
-
-  return -1; // Return -1 if the nested key is not found in the JSON string.
 }
 
 export function generateDescriptions(key: string, data: object) {
