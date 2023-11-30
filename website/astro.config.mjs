8ee538aa23698f48280489356354047b73f06258
Tejas Kumar <tejas@tejas.qa>
2023-11-30 13:07:17 +0100

    Add sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    solidJs(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), solidJs(), sitemap()]
 });
