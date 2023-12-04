de23a20f4d4f1d8b29da63d39171a9f12a574247
Tejas Kumar, but for CrabNebula <125391648+tejas-crabnebula@users.noreply.github.com>
2023-12-04 11:37:03 +0100

    Website (#120)
    * Add website
    
    * Update todos
    
    * Add dropdowns, baby Nova, RWD fixes
    
    * Add links
    
    * Add sitemap
    
    * Add sitemap
    
    * Close all dropdowns on click of document
    
    * Perf optimizations
    
    * More perf
    
    * Use webp
    
    * Accessibility
    
    * Add newsletter logic
    
    * Update README
    
    * Favor `class:list` over `clsx`
    
    * Remove unused class
    
    * Add ToS link
    
    * Add Netlify adapter
    
    * Remove cslx
    
    * Update screenshot
    
    * Add footer socials
    
    * Make button shorter
    
    * Adjust padding
    
    * Open socials in new tab
    
    * Fix footer links
    
    * Add link hover color
    
    * Hover social icons
    
    * Remove console.log
    
    * Try SSR
    
    * Make links accessible
    
    * Add Fathom
    
    ---------
    
    Co-authored-by: Tejas Kumar <tejas@tejas.qa>

---
+++ website/src/components/MobileNav.tsx
@@ -0,0 +1,39 @@
+import { Match, Show, Switch, createSignal } from "solid-js";
+import { BurgerIcon } from "./BurgerIcon";
+import { NavAccordion } from "./NavAccordion";
+import { CloseBurgerIcon } from "./CloseBurgerIcon";
+
+export const MobileNav = () => {
+  const [isActive, setIsActive] = createSignal(false);
+
+  const toggle = () => {
+    setIsActive(!isActive());
+  };
+
+  return (
+    <>
+      <button
+        aria-label="Toggle mobile navigation"
+        class="lg:hidden"
+        onClick={() => toggle()}
+      >
+        <Switch>
+          <Match when={isActive()}>
+            <CloseBurgerIcon />
+          </Match>
+          <Match when={!isActive()}>
+            <BurgerIcon />
+          </Match>
+        </Switch>
+      </button>
+      <Show when={isActive()}>
+        <div
+          onclick={() => setIsActive(false)}
+          class="fixed z-10 px-4 py-8 top-[104px] left-0 w-full h-[calc(100vh-104px)] bg-black bg-opacity-90 backdrop-blur-xl"
+        >
+          <NavAccordion />
+        </div>
+      </Show>
+    </>
+  );
+};
