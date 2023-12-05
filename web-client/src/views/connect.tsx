98056ecf607b3b09b82c9cece30eb9d4a3f68fdc
Atila Fassina <atila@crabnebula.dev>
2023-12-05 11:18:57 +0100

    fix background, favicon, and fonts

--- web-client/src/views/connect.tsx
+++ web-client/src/views/connect.tsx
@@ -126,7 +126,7 @@ export default function Connect() {
       </footer>
 
       <div class="surf-container">
-        <img class="bg-surface static" src="/bg.jpeg" alt="" />
+        <img class="bg-surface static" src="/app/bg.jpeg" alt="" />
       </div>
 
       <ConnectionFailedDialog
