b673605d6348f6a95d7ee552921d664710401a2c
Atila Fassina <atila@crabnebula.dev>
2023-12-05 10:36:47 +0100

    change Shiki CDN routing

--- web-client/src/lib/code-highlight.ts
+++ web-client/src/lib/code-highlight.ts
@@ -67,8 +67,8 @@ export function textToHtml(
 }
 
 export async function createHighlighter() {
-  setCDN("/shiki/");
-  const responseWasm = await fetch("/shiki/onig.wasm");
+  setCDN("/app/shiki/");
+  const responseWasm = await fetch("/app/shiki/onig.wasm");
   setWasm(responseWasm);
 
   return await getHighlighter({
