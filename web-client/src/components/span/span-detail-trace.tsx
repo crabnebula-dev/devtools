3e53722c6cb47643e31aa48a8b5e9df9cd2b4b58
Johann Derdak <johann@crabnebula.dev>
2023-12-19 16:56:00 +0100

    fix: lint warnings and improve readability

--- web-client/src/components/span/span-detail-trace.tsx
+++ web-client/src/components/span/span-detail-trace.tsx
@@ -1,5 +1,6 @@
 import { For } from "solid-js";
 import { UiSpan } from "~/lib/span/format-spans-for-ui";
+import { Span } from "~/lib/connection/monitor";
 import {
   computeWaterfallStyle,
   computeSlices,
@@ -16,13 +17,6 @@ export function SpanDetailTrace(props: {
     longest: number;
   };
 }) {
-  const busy = () =>
-    props.span.original.enters.reduce((acc, enter, i) => {
-      const exit = props.span.original.exits[i];
-
-      return acc + (exit.timestamp - enter.timestamp);
-    }, 0);
-
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -45,41 +39,7 @@ export function SpanDetailTrace(props: {
             >
               {/* Slices is "time slices" as in multiple entry points to a given span */}
               <For each={computeSlices(props.span.original)}>
-                {(slice) => (
-                  <Popover.Root>
-                    <Popover.Trigger
-                      class="absolute bg-teal-500 top-0 left-0 h-full hover:bg-teal-300"
-                      style={{
-                        width: `${slice.width}%`,
-                        "margin-left": `${slice.marginLeft}%`,
-                      }}
-                    ></Popover.Trigger>
-                    <Popover.Portal>
-                      <Popover.Content
-                        class="z-50 bg-gray-700 rounded-sm drop-shadow-2xl px-2 py-1 focus:outline-none grid grid-cols-2"
-                        style={{
-                          "max-width": "min(calc(100vw - 16px), 380px)",
-                        }}
-                      >
-                        <Popover.Arrow />
-                        <span>Time</span>
-                        <span>
-                          {((slice.exited - slice.entered) / 1e6).toFixed(3)}ms
-                        </span>
-                        <span>Thread</span>
-                        <span>{slice.threadID}</span>
-                        <span>Start</span>
-                        <span>
-                          {getDetailedTime(new Date(slice.entered / 1e6))}
-                        </span>
-                        <span>End</span>
-                        <span>
-                          {getDetailedTime(new Date(slice.exited / 1e6))}
-                        </span>
-                      </Popover.Content>
-                    </Popover.Portal>
-                  </Popover.Root>
-                )}
+                {(slice) => <SpanDetailSlice slice={slice} />}
               </For>
             </div>
           </div>
@@ -87,17 +47,74 @@ export function SpanDetailTrace(props: {
         <td class="py-1 px-4">{props.span.time.toFixed(2)}ms</td>
       </Popover.Trigger>
       <Popover.Portal>
+        <SpanDetailPopOverContent span={props.span} />
+      </Popover.Portal>
+    </Popover.Root>
+  );
+}
+
+function SpanDetailPopOverContent(props: { span: UiSpan }) {
+  const busy = (span: Span) =>
+    span.enters.reduce((acc, enter, i) => {
+      const exit = span.exits[i];
+
+      return acc + (exit.timestamp - enter.timestamp);
+    }, 0);
+
+  return (
+    <Popover.Content
+      class="z-50 bg-gray-700 rounded-sm drop-shadow-2xl px-2 py-1 focus:outline-none grid grid-cols-2"
+      style={{ "max-width": "min(calc(100vw - 16px), 380px)" }}
+    >
+      <Popover.Arrow />
+      <span>{props.span.name}</span>
+      <span />
+      <span>Busy</span>
+      <span>{(busy(props.span.original) / 1e6).toFixed(3)}ms</span>
+      <span>Idle</span>
+      <span>
+        {(props.span.time - busy(props.span.original) / 1e6).toFixed(3)}ms
+      </span>
+    </Popover.Content>
+  );
+}
+
+function SpanDetailSlice(props: {
+  slice: {
+    entered: number;
+    exited: number;
+    threadID: number;
+    width: number;
+    marginLeft: number;
+  };
+}) {
+  return (
+    <Popover.Root>
+      <Popover.Trigger
+        class="absolute bg-teal-500 top-0 left-0 h-full hover:bg-teal-300"
+        style={{
+          width: `${props.slice.width}%`,
+          "margin-left": `${props.slice.marginLeft}%`,
+        }}
+      />
+      <Popover.Portal>
         <Popover.Content
           class="z-50 bg-gray-700 rounded-sm drop-shadow-2xl px-2 py-1 focus:outline-none grid grid-cols-2"
-          style={{ "max-width": "min(calc(100vw - 16px), 380px)" }}
+          style={{
+            "max-width": "min(calc(100vw - 16px), 380px)",
+          }}
         >
           <Popover.Arrow />
-          <span>{props.span.name}</span>
-          <span />
-          <span>Busy</span>
-          <span>{(busy() / 1e6).toFixed(3)}ms</span>
-          <span>Idle</span>
-          <span>{(props.span.time - busy() / 1e6).toFixed(3)}ms</span>
+          <span>Time</span>
+          <span>
+            {((props.slice.exited - props.slice.entered) / 1e6).toFixed(3)}ms
+          </span>
+          <span>Thread</span>
+          <span>{props.slice.threadID}</span>
+          <span>Start</span>
+          <span>{getDetailedTime(new Date(props.slice.entered / 1e6))}</span>
+          <span>End</span>
+          <span>{getDetailedTime(new Date(props.slice.exited / 1e6))}</span>
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
