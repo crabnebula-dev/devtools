cb1ddf12969919ae22dc4303018b395e6fcb01b5
Jonas Kruckenberg <118265418+CrabNejonas@users.noreply.github.com>
2023-12-19 16:01:38 +0100

    Update span-detail-trace.tsx

--- web-client/src/components/span/span-detail-trace.tsx
+++ web-client/src/components/span/span-detail-trace.tsx
@@ -16,8 +16,15 @@ export function SpanDetailTrace(props: {
     longest: number;
   };
 }) {
+  const busy = () => props.span.original.enters.reduce((acc, enter, i) => {
+    const exit = props.span.original.exits[i];
+
+    return acc + (exit.timestamp - enter.timestamp)
+  }, 0)
+
   return (
-    <tr class="even:bg-nearly-invisible cursor-pointer hover:bg-[#ffffff05] even:hover:bg-[#ffffff10]">
+      <Popover.Root>
+        <Popover.Trigger class="even:bg-nearly-invisible cursor-pointer hover:bg-[#ffffff05] even:hover:bg-[#ffffff10]" as="tr">
       <td class="py-1 px-4">{props.span.name}</td>
       <td class="py-1 px-4 relative w-[70%]">
         <div class="relative">
@@ -37,7 +44,7 @@ export function SpanDetailTrace(props: {
               {(slice) => (
                 <Popover.Root>
                   <Popover.Trigger
-                    class="absolute bg-teal-500 top-0 left-0 h-full"
+                    class="absolute bg-teal-500 top-0 left-0 h-full hover:bg-teal-300"
                     style={{
                       width: `${slice.width}%`,
                       "margin-left": `${slice.marginLeft}%`,
@@ -45,22 +52,18 @@ export function SpanDetailTrace(props: {
                   ></Popover.Trigger>
                   <Popover.Portal>
                     <Popover.Content
-                      class="z-50 bg-gray-800 rounded-sm drop-shadow-2xl p-1 focus:outline-none"
-                      style={{ "max-width": "min(calc(100vw - 16px), 380px)" }}
+                        class="z-50 bg-gray-700 rounded-sm drop-shadow-2xl px-2 py-1 focus:outline-none grid grid-cols-2"
+                        style={{"max-width": "min(calc(100vw - 16px), 380px)"}}
                     >
-                      <Popover.Arrow />
-                      <p>
-                        time:{" "}
-                        {((slice.exited - slice.entered) / 1e6).toFixed(3)}ms
-                      </p>
-                      <p>on thread: {slice.threadID}</p>
-                      <p>
-                        entered:{" "}
-                        {getDetailedTime(new Date(slice.entered / 1e6))}
-                      </p>
-                      <p>
-                        exited: {getDetailedTime(new Date(slice.exited / 1e6))}
-                      </p>
+                      <Popover.Arrow/>
+                      <span>Time</span>
+                      <span>{((slice.exited - slice.entered) / 1e6).toFixed(3)}ms</span>
+                      <span>Thread</span>
+                      <span>{slice.threadID}</span>
+                      <span>Start</span>
+                      <span>{getDetailedTime(new Date(slice.entered / 1e6))}</span>
+                      <span>End</span>
+                      <span>{getDetailedTime(new Date(slice.exited / 1e6))}</span>
                     </Popover.Content>
                   </Popover.Portal>
                 </Popover.Root>
@@ -70,6 +73,19 @@ export function SpanDetailTrace(props: {
         </div>
       </td>
       <td class="py-1 px-4">{props.span.time.toFixed(2)}ms</td>
-    </tr>
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content
+              class="z-50 bg-gray-700 rounded-sm drop-shadow-2xl px-2 py-1 focus:outline-none grid grid-cols-2"
+              style={{"max-width": "min(calc(100vw - 16px), 380px)"}}>
+            <Popover.Arrow/>
+            <span>{props.span.name}</span><span />
+            <span>Busy</span>
+            <span>{(busy() / 1e6).toFixed(3)}ms</span>
+            <span>Idle</span>
+            <span>{(props.span.time - busy() / 1e6).toFixed(3)}ms</span>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
   );
 }
