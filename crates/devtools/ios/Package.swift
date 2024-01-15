264e5c518a5c3fe5bd17ad7f190e32061a6de02a
Lucas Nogueira <lucas@crabnebula.dev>
2024-01-15 15:24:33 -0300

    fix iOS

--- crates/devtools/ios/Package.swift
+++ crates/devtools/ios/Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "tauri-plugin-devtools",
+    name: "devtools",
     platforms: [
         .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "tauri-plugin-devtools",
+            name: "devtools",
             type: .static,
-            targets: ["tauri-plugin-devtools"]),
+            targets: ["devtools"]),
     ],
     dependencies: [
         .package(name: "Tauri", path: "../.tauri/tauri-api")
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "tauri-plugin-devtools",
+            name: "devtools",
             dependencies: [
                 .byName(name: "Tauri")
             ],
