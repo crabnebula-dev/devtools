17339fae7a40d165edf6cdf62f74339659c619d1
Atila Fassina <atila@crabnebula.dev>
2024-01-23 20:02:32 +0100

    switch `.jpeg` for `.webp`

--- clients/web/src/views/dashboard/layout.tsx
+++ clients/web/src/views/dashboard/layout.tsx
@@ -33,7 +33,7 @@ export default function Layout(props: Props) {
         <div class="overflow-hidden absolute inset-0 w-full h-full -z-10">
           <img
             class="relative opacity-30 scale-110 bg-surface bg-surface"
-            src="/bg.jpeg"
+            src="/app/bg.webp"
             aria-hidden
           />
         </div>
