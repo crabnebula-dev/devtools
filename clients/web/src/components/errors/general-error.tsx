2a96c9a4091f670dae2c44ada4713fed563e7d10
Atila Fassina <atila@crabnebula.dev>
2024-01-29 09:44:06 +0100

    update general error message

--- clients/web/src/components/errors/general-error.tsx
+++ clients/web/src/components/errors/general-error.tsx
@@ -2,7 +2,7 @@ export function GeneralError() {
   return (
     <>
       <h1 class="text-red-400 font-semibold text-6xl">
-        Oops! We can come back...{" "}
+        Oops! We can’t come back...{" "}
       </h1>
       <div class="text-3xl pt-8">
         <p>Something terrible happened.</p>
