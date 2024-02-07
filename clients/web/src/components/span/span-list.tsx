f21226aab48fe4930dda84b5b67fc69cd13bd8af
Johann Derdak <johann@crabnebula.dev>
2024-02-07 11:05:53 +0100

    fix: revert changes to calls

--- clients/web/src/components/span/span-list.tsx
+++ clients/web/src/components/span/span-list.tsx
@@ -1,5 +1,5 @@
 import { useSearchParams } from "@solidjs/router";
-import { For, Index, createEffect, createMemo } from "solid-js";
+import { For } from "solid-js";
 import { createStore } from "solid-js/store";
 import type { UiSpan } from "~/lib/span/format-spans-for-ui";
 import { getColumnDirection } from "~/lib/span/get-column-direction";
@@ -12,7 +12,6 @@ import {
   computeWaterfallStyle,
 } from "~/lib/span/normalize-spans";
 import clsx from "clsx";
-import { createVirtualizer } from "@tanstack/solid-virtual";
 
 export type SortDirection = "asc" | "desc";
 
@@ -30,7 +29,6 @@ type Column = {
 export function SpanList() {
   const callsContext = useCalls();
   const spans = callsContext.spans;
-  let virtualList: HTMLTableElement | undefined;
 
   const [columnSort, setColumnSort] = createStore<ColumnSort>({
     name: "initiated",
@@ -45,6 +43,23 @@ export function SpanList() {
   ] satisfies Column[];
 
   const [, setSearchParams] = useSearchParams();
+
+  const filteredSpans = () => {
+    const filteredSpans: UiSpan[] = [];
+    spans.forEach((span) => {
+      if (span.kind) {
+        filteredSpans.push(span);
+      }
+    });
+    return [...filteredSpans].sort(spanSort);
+  };
+
+  const sortColumn = (name: SortableColumn) => {
+    setColumnSort({
+      name,
+      direction: getColumnDirection(columnSort, name),
+    });
+  };
 
   const spanSort = (a: UiSpan, b: UiSpan) => {
     const columnName = columnSort.name;
@@ -66,157 +81,98 @@ export function SpanList() {
     }
   };
 
-  const computeFilteredSpans = () => {
-    const filteredSpans: UiSpan[] = [];
-    spans.forEach((span) => {
-      if (span.kind) {
-        filteredSpans.push(span);
-      }
-    });
-
-    return [...filteredSpans].sort(spanSort);
-  };
-
-  const filteredSpans = createMemo(() => computeFilteredSpans());
-
-  const count = createMemo(() => {
-    return filteredSpans()?.length ?? 0;
-  });
-
-  const sortColumn = (name: SortableColumn) => {
-    setColumnSort({
-      name,
-      direction: getColumnDirection(columnSort, name),
-    });
-    virtualizer.measure();
-  };
-
-  const virtualizer = createVirtualizer({
-    count: 0,
-    getScrollElement: () => virtualList ?? null,
-    estimateSize: () => 50,
-    overscan: 5,
-  });
-
-  createEffect(() => {
-    const newCount = count();
-    virtualizer.setOptions({ ...virtualizer.options, count: newCount });
-    virtualizer.measure();
-  });
-
   return (
-    <div ref={virtualList} class="overflow-auto max-h-full h-full relative">
-      <div style={{ height: `${virtualizer.getTotalSize()}px` }}>
-        <table class="w-full table-fixed">
-          <thead>
-            <tr class="text-left">
-              <For each={columns}>
-                {(column) => {
-                  return (
-                    <th
-                      tabIndex="0"
-                      onKeyDown={(e) => {
-                        if (
-                          [" ", "Enter"].includes(e.key) &&
-                          column.isSortable
-                        ) {
-                          sortColumn(column.name);
-                        }
-                      }}
-                      onClick={() =>
-                        column.isSortable && sortColumn(column.name)
-                      }
-                      class={`p-1 cursor-pointer hover:bg-[#ffffff09] ${
-                        column.name === "time" || column.name === "initiated"
-                          ? "w-2/12" // time and initiated
-                          : column.name === "name"
-                          ? "w-3/12" // name
-                          : "w-5/12" // waterfall
-                      }`}
+    <table class="w-full table-fixed">
+      <thead>
+        <tr class="text-left">
+          <For each={columns}>
+            {(column) => {
+              console.log(column);
+              return (
+                <th
+                  tabIndex="0"
+                  onKeyDown={(e) => {
+                    if ([" ", "Enter"].includes(e.key) && column.isSortable) {
+                      sortColumn(column.name);
+                    }
+                  }}
+                  onClick={() => column.isSortable && sortColumn(column.name)}
+                  class={`p-1 cursor-pointer hover:bg-[#ffffff09] ${
+                    column.name === "time" || column.name === "initiated"
+                      ? "w-2/12" // time and initiated
+                      : column.name === "name"
+                      ? "w-3/12" // name
+                      : "w-5/12" // waterfall
+                  }`}
+                >
+                  <div class="flex uppercase select-none items-center gap-2">
+                    {column.name}
+                    {columnSort.name === column.name && column.isSortable && (
+                      <SortCaret direction={columnSort.direction} />
+                    )}
+                  </div>
+                </th>
+              );
+            }}
+          </For>
+        </tr>
+      </thead>
+      <tbody>
+        <For each={filteredSpans()}>
+          {(span) => {
+            return (
+              <tr
+                onClick={() => setSearchParams({ span: String(span.id) })}
+                class="even:bg-nearly-invisible cursor-pointer hover:bg-[#ffffff05] even:hover:bg-[#ffffff10]"
+              >
+                <td class="p-1 overflow-hidden text-ellipsis" title={span.name}>
+                  {span.name}
+                </td>
+                <td
+                  class="p-1 overflow-hidden text-ellipsis"
+                  title={getTime(new Date(span.initiated))}
+                >
+                  {getTime(new Date(span.initiated))}
+                </td>
+                <td
+                  class="p-1 overflow-hidden text-ellipsis"
+                  title={`${span.time.toFixed(2)} ms`}
+                >
+                  {span.time.toFixed(2)}ms
+                </td>
+                <td class="p-1 relative overflow-hidden">
+                  <div class="relative w-[90%]">
+                    <div class="bg-gray-800 w-full absolute rounded-sm h-2" />
+                    <div
+                      class={clsx(
+                        "relative rounded-sm h-2",
+                        computeColorClassName(
+                          span.original.closedAt - span.original.createdAt,
+                          callsContext.durations.durations.average
+                        )
+                      )}
+                      style={computeWaterfallStyle(
+                        span,
+                        callsContext.durations.durations.start,
+                        callsContext.durations.durations.end
+                      )}
                     >
-                      <div class="flex uppercase select-none items-center gap-2">
-                        {column.name}
-                        {columnSort.name === column.name &&
-                          column.isSortable && (
-                            <SortCaret direction={columnSort.direction} />
-                          )}
-                      </div>
-                    </th>
-                  );
-                }}
-              </For>
-            </tr>
-          </thead>
-          <tbody>
-            <For each={virtualizer.getVirtualItems()}>
-              {(virtualRow, index) => {
-                const span = () => filteredSpans()[virtualRow.index];
-                return (
-                  <tr
-                    onClick={() => setSearchParams({ span: String(span().id) })}
-                    class="even:bg-nearly-invisible cursor-pointer hover:bg-[#ffffff05] even:hover:bg-[#ffffff10]"
-                    data-id={virtualRow.index}
-                    style={{
-                      height: `${virtualRow.size}px`,
-                      transform: `translateY(${
-                        virtualRow.start - index() * virtualRow.size
-                      }px)`,
-                    }}
-                  >
-                    <td
-                      class="p-1 overflow-hidden text-ellipsis"
-                      title={span().name}
-                    >
-                      {span().name}
-                    </td>
-                    <td
-                      class="p-1 overflow-hidden text-ellipsis"
-                      title={getTime(new Date(span().initiated))}
-                    >
-                      {getTime(new Date(span().initiated))}
-                    </td>
-                    <td
-                      class="p-1 overflow-hidden text-ellipsis"
-                      title={`${span().time.toFixed(2)} ms`}
-                    >
-                      {span().time.toFixed(2)}ms
-                    </td>
-                    <td class="p-1 relative overflow-hidden">
-                      <div class="relative w-[90%]">
-                        <div class="bg-gray-800 w-full absolute rounded-sm h-2" />
-                        <div
-                          class={clsx(
-                            "relative rounded-sm h-2",
-                            computeColorClassName(
-                              span().original.closedAt -
-                                span().original.createdAt,
-                              callsContext.durations.durations.average
-                            )
-                          )}
-                          style={computeWaterfallStyle(
-                            span(),
-                            callsContext.durations.durations.start,
-                            callsContext.durations.durations.end
-                          )}
-                        >
-                          <For each={computeSlices(span().original)}>
-                            {(slice) => (
-                              <div
-                                class="absolute top-0 left-0 bg-black bg-opacity-10 h-full"
-                                style={slice}
-                              />
-                            )}
-                          </For>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              }}
-            </For>
-          </tbody>
-        </table>
-      </div>
-    </div>
+                      <For each={computeSlices(span.original)}>
+                        {(slice) => (
+                          <div
+                            class="absolute top-0 left-0 bg-black bg-opacity-10 h-full"
+                            style={slice}
+                          />
+                        )}
+                      </For>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            );
+          }}
+        </For>
+      </tbody>
+    </table>
   );
 }
