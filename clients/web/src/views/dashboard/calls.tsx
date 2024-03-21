837cad41e68b9ac5b0073849a238e4cb96725c74
Robin <robin@crabnebula.dev>
2024-03-21 13:55:53 +0100

    hack: support error filter

--- clients/web/src/views/dashboard/calls.tsx
+++ clients/web/src/views/dashboard/calls.tsx
@@ -8,27 +8,41 @@ import { useMonitor } from "~/context/monitor-provider";
 import { CallsClearButton } from "~/components/calls/topbar/calls-clear-button";
 import { filterSpans } from "~/lib/span/filter-spans";
 import { Span } from "~/lib/connection/monitor";
-import { createMemo } from "solid-js";
+import { createMemo, createSignal } from "solid-js";
+import { FilterToggle } from "~/components/filter-toggle";
 
 function Calls() {
   const { monitorData } = useMonitor();
 
+  const [showErrors, toggleErrorFilter] = createSignal(false);
+
   let spanProcessingPointer = 0;
 
   const filteredCalls = createMemo<Span[]>((alreadyFiltered) => {
+    const onlyErrors = showErrors();
     const [filteredCalls, newPointer] = filterSpans(
       alreadyFiltered,
       spanProcessingPointer,
       monitorData.spans,
     );
     spanProcessingPointer = newPointer;
-    return filteredCalls;
+    // TODO: this is not reactive when spans are marked as error later.
+    return filteredCalls.filter(
+      (s) => !onlyErrors || s.hasChildError || s.hasError,
+    );
   }, []);
 
   return (
     <div class="h-[calc(100%-var(--toolbar-height))]">
       <Toolbar>
         <CallsClearButton />
+        <FilterToggle
+          defaultPressed={showErrors()}
+          aria-label="errors only"
+          changeHandler={() => toggleErrorFilter((prev) => !prev)}
+        >
+          <span>Errors only</span>
+        </FilterToggle>
         <span>Displayed calls: {filteredCalls().length}</span>
         <span>Running calls: {monitorData.durations.openSpans}</span>
         <CallsScaleSlider />
