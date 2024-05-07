3962f8cf4afc6438124237638d521ceee2b5f6a4
Johann Derdak <johann@crabnebula.dev>
2024-05-07 07:17:08 +0200

    fix: add title to new linked source svg

--- clients/web/src/components/icons/migrate--alt.tsx
+++ clients/web/src/components/icons/migrate--alt.tsx
@@ -6,6 +6,7 @@ export default function MigrateAlt() {
       height="100%"
       viewBox="0 0 32 32"
     >
+      <title>Open linked call</title>
       <path
         d="M28 4H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Zm0 22H12v-6h-2v6H4v-9h16.2l-3.6 3.6L18 22l6-6-6-6-1.4 1.4 3.6 3.6H4V6h6v6h2V6h16Z"
         fill="currentColor"
