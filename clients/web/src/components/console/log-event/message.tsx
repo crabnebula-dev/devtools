180774cc772dfc48e0aa900ebf65c99db69eddae
Alex Lohr <alex@crabnebula.dev>
2024-06-06 12:21:55 +0200

    feat: collapse, pre formatting, DT-180

--- clients/web/src/components/console/log-event/message.tsx
+++ clients/web/src/components/console/log-event/message.tsx
@@ -1,5 +1,12 @@
 import type { ProcessedLogEvent } from "~/lib/console/process-log-event-for-view";
-import { Show } from "solid-js";
+import {
+  createMemo,
+  createSignal,
+  onCleanup,
+  onMount,
+  Show,
+  untrack,
+} from "solid-js";
 import { A } from "@solidjs/router";
 import MigrateAlt from "~/components/icons/migrate--alt";
 import { useConnection } from "~/context/connection-provider";
@@ -9,9 +16,28 @@ export function Message(props: {
   processedEvent: ProcessedLogEvent;
   showLinks: boolean | undefined;
 }) {
+  const [ref, setRef] = createSignal<HTMLSpanElement>();
+  const [overflows, setOverflows] = createSignal(false);
+  const [collapse, setCollapse] = createSignal(true);
+  const [pre, setPre] = createSignal(false);
+
+  onMount(() => {
+    const measure = () =>
+      setOverflows(
+        (ref()?.offsetHeight || 0) > (ref()?.parentElement?.offsetHeight || 0),
+      );
+    measure();
+    window.addEventListener("resize", measure);
+    onCleanup(() => {
+      window.removeEventListener("resize", measure);
+    });
+  });
+
   const {
     connectionStore: { host, port },
   } = useConnection();
+
+  const message = createMemo(() => props.processedEvent.message);
 
   return (
     <Show when={props.processedEvent.message.length}>
@@ -27,27 +53,74 @@ export function Message(props: {
           </A>
         </Show>
         <span
-          style="block-overflow: ellipsis"
-          class="line-clamp-3 max-h-16"
-          title={props.processedEvent.message}
+          classList={{
+            "group block max-h-[60px]": true,
+            "overflow-hidden": collapse(),
+          }}
         >
-          {makeBreakable(props.processedEvent.message, 150)}
-        </span>
-        <Show when={props.processedEvent.message.length > 50}>
-          <button
-            class="ml-2"
-            onClick={() =>
-              navigator.clipboard.writeText(props.processedEvent.message)
-            }
-            title="copy full message to clipboard"
+          <span
+            classList={{
+              [`block relative bg-black z-50
+                before:shadow-[0_0_1rem_rgba(255,255,255,0.2)]
+                before:absolute before:rounded-lg
+                before:bg-black before:left-[-1rem]
+                before:top-[-1rem] before:right-[-1rem]
+                before:bottom-[-1rem] before:z-0 before:pointer-events-none`]:
+                !collapse(),
+              "whitespace-pre-wrap": pre(),
+            }}
+            title={message()}
+            ref={setRef}
           >
-            <img
-              class="h-4 w-4"
-              src="/icons/copy.svg"
-              alt="copy full message to clipboard"
-            />
-          </button>
-        </Show>
+            <span
+              classList={{
+                "block max-h-[50vh] overflow-auto relative": !collapse(),
+              }}
+            >
+              <Show when={message().length > 40} fallback={message()}>
+                {makeBreakable(message())}
+              </Show>
+            </span>
+            <Show when={overflows() || !collapse()}>
+              <span class="hidden group-hover:block absolute bg-black right-2 top-0 z-51 pl-1 pb-1 ">
+                <button
+                  onClick={() => navigator.clipboard.writeText(message())}
+                  title="copy full message to clipboard"
+                >
+                  <img
+                    class="h-4 w-4"
+                    src="/icons/copy.svg"
+                    alt="copy full message to clipboard"
+                  />
+                </button>
+                <Show when={untrack(() => /\r|\n|\t|\s\s+/.test(message()))}>
+                  <button
+                    class="ml-1"
+                    onClick={() => setPre(!pre())}
+                    title="toggle show indentation"
+                  >
+                    <img
+                      class="h-4 w-4"
+                      src="/icons/code.svg"
+                      alt="toggle show indentation"
+                    />
+                  </button>
+                </Show>
+                <button
+                  class="ml-1"
+                  onClick={() => setCollapse(!collapse())}
+                  title="toggle collapse entry"
+                >
+                  <img
+                    class="h-4 w-4"
+                    src="/icons/code.svg"
+                    alt="toggle collapse entry"
+                  />
+                </button>
+              </span>
+            </Show>
+          </span>
+        </span>
       </span>
     </Show>
   );
