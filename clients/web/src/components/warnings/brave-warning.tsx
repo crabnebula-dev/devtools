110fe733759f5876fe03562dc8ea6c6cfaf810ed
Johann Derdak <johann@crabnebula.dev>
2024-06-10 16:03:26 +0900

    fix: assume that when `navigator.brave.isBrave()` is set and returns any promise, Brave browser is running

--- clients/web/src/components/warnings/brave-warning.tsx
+++ clients/web/src/components/warnings/brave-warning.tsx
@@ -1,4 +1,4 @@
-import { JSXElement, Show, Suspense } from "solid-js";
+import { JSXElement, Show } from "solid-js";
 import { Info } from "../icons/info";
 
 // The Brave browser exports a navigator function to determine whether we are using it.
@@ -11,39 +11,37 @@ declare global {
 }
 
 export function BraveWarning() {
-  const isBrave = (navigator.brave && navigator.brave.isBrave()) || false;
+  const isBrave = !!navigator.brave?.isBrave();
 
   return (
-    <Suspense>
-      <Show when={isBrave}>
-        <section class="text-base border border-neutral-800 rounded-lg p-3 shadow-lg shadow-navy-600 my-2 break-words">
-          <p class="text-2xl text-slate-200">
-            <Info /> Info
-          </p>
-          <h1 class="text-slate-200 text-xl pb-2 font-semibold">
-            We noticed you are using the <Highlight>Brave browser</Highlight>
-          </h1>
-          <p class="pb-2">
-            Users have reported that requests to{" "}
-            <Highlight>localhost will be blocked</Highlight> by the{" "}
-            <Highlight>Brave browser shield</Highlight> by default.
-          </p>
-          <p>
-            We have a little section in our docs on how to add an exception to
-            this rule for <Highlight>DevTools Web</Highlight>:{" "}
-            <Highlight>
-              <a
-                class="underline"
-                href="https://docs.crabnebula.dev/devtools/troubleshooting/web/brave-browser"
-                target="_blank"
-              >
-                docs.crabnebula.dev/devtools/troubleshooting/web/brave-browser
-              </a>
-            </Highlight>
-          </p>
-        </section>
-      </Show>
-    </Suspense>
+    <Show when={isBrave}>
+      <section class="text-base border border-neutral-800 rounded-lg p-3 shadow-lg shadow-navy-600 my-2 break-words">
+        <p class="text-2xl text-slate-200">
+          <Info /> Info
+        </p>
+        <h1 class="text-slate-200 text-xl pb-2 font-semibold">
+          We noticed you are using the <Highlight>Brave browser</Highlight>
+        </h1>
+        <p class="pb-2">
+          Users have reported that requests to{" "}
+          <Highlight>localhost will be blocked</Highlight> by the{" "}
+          <Highlight>Brave browser shield</Highlight> by default.
+        </p>
+        <p>
+          We have a little section in our docs on how to add an exception to
+          this rule for <Highlight>DevTools Web</Highlight>:{" "}
+          <Highlight>
+            <a
+              class="underline"
+              href="https://docs.crabnebula.dev/devtools/troubleshooting/web/brave-browser"
+              target="_blank"
+            >
+              docs.crabnebula.dev/devtools/troubleshooting/web/brave-browser
+            </a>
+          </Highlight>
+        </p>
+      </section>
+    </Show>
   );
 }
 
