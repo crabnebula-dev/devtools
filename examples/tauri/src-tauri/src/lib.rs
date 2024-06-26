25c2c242ce213cc771025889c5828c09404e4832
Amr Bashir <amr@crabnebula.dev>
2024-06-26 07:01:29 +0300

    correct crate name

--- examples/tauri/src-tauri/src/lib.rs
+++ examples/tauri/src-tauri/src/lib.rs
@@ -66,7 +66,7 @@ async fn test1(
 /// Panics if the application cannot be built.
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let devtools = tauri_plugin_devtools::init();
+    let devtools = devtools::init();
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![test1])
