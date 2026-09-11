4fc9ec14ccab09dc1f4a67601faf46f78d2a0cac
Jonas Kruckenberg <118265418+CrabNejonas@users.noreply.github.com>
2026-09-11 11:38:01 +0000

    fix: dont drop events in buffer (#227)
    Increase span buffer size to avoid dropped spans and UX issues.
    
    ---
    
    After more testing and weighing the options I decided it makes more sense to just increase the limit for the fixed-size EventBuffer structs to be fitted better.
    The reasons are:
    
        Later clients missing out on all previously emitted data is a bad UX bc it looks the instrumentation is broken even if his would be technically correct behaviour
        If you leave your app running while having the instrumentation running but never attaching a client using Vec will consume unbounded memory. I expect this to happen a lot as commenting out the instrumentation code is very annoying and users will probably only open the client when they actually need it
        From testing 2048 should be enough to fit all events even under most significant event pressure.
    
    Alternatives
    
    Originally I had considered adjusting the publish interval based on some metric so that it would be reduced the fuller the internal buffers get. However, due to the way the tokio::mpsc channel works this wouldn't really adjust the timing by much anyway and just add extra complexity

--- crates/devtools-core/src/aggregator.rs
+++ crates/devtools-core/src/aggregator.rs
@@ -36,8 +36,8 @@ pub struct Aggregator {
     /// Up to 512 events are retained before the oldest will be dropped.
     logs: EventBuf<LogEvent, 512>,
     /// Buffered span events.
-    /// Up to 512 events are retained before the oldest will be dropped.
-    spans: EventBuf<SpanEvent, 512>,
+    /// Up to 2048 events are retained before the oldest will be dropped.
+    spans: EventBuf<SpanEvent, 2048>,
 
     /// All connected clients
     watchers: Vec<Watcher>,
